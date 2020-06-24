19f6c381343554732e2fd9427d64ad3b68a296ba
Lincoln Baker <lbaker@chef.io>
2020-06-23 22:51:49 -0500

    test a change
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- oc-chef-pedant/lib/pedant/rspec/cookbook_util.rb
+++ oc-chef-pedant/lib/pedant/rspec/cookbook_util.rb
@@ -54,8 +54,10 @@ module Pedant
           # functions that make use of Tempfile. Since we want the
           # entire contents, rewind before read.
           file.rewind
-puts"\n\ncookbook_util -\nupload_url: #{upload_url}\nheaders: #{headers}"
 
+uri = URI.parse(upload_url)
+headers['Host'] ? :noop : headers['Host'] = "#{uri.hostname}:#{uri.port}"
+puts"\n\ncookbook_util -\nupload_url: #{upload_url}\nheaders: #{headers}"
 
           ensure_2xx(put(upload_url, admin_user, :payload => file.read,
                          :headers => headers))
